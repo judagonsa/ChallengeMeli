648c1bc4e6e2a759b600e108a479c81a2a4c39d6
judagonsa <judagonsa8@gmail.com>
2022-06-06 19:06:39 -0500

    User interaction full

--- ChallengeLibre/ListProducts/ListProductsViewController.swift
+++ ChallengeLibre/ListProducts/ListProductsViewController.swift
@@ -7,16 +7,46 @@
 
 import UIKit
 
-class ListProductsViewController: UIViewController {
+class ListProductsViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var txtSearch: UISearchBar!
+    @IBOutlet weak var txtAddressSend: UILabel!
+    
 
     var arrayProducts: [ProductModel.Data] = []
+    var txtTitleSearch = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        txtSearch.delegate = self
+        txtSearch.text = txtTitleSearch
         
     }
-
     
-
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        if searchBar == txtSearch {
+            let storyBoard = UIStoryboard(name: "SearchProductView", bundle: nil)
+            let controller = storyBoard.instantiateViewController(withIdentifier: "SearchProductViewController") as! SearchProductViewController
+            controller.txtTitleSearch = txtSearch.text!
+            controller.isSearchAgain = true
+            self.present(controller, animated: false, completion: nil)
+        }
+    }
+    
+    @IBAction func btnBackAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func btnCarShippingAction(_ sender: Any) {
+        showDefaultAlert(title: "Atención", message: "Acción de ver carrito en desarrollo", view: self)
+    }
+    
+    @IBAction func btnAddressAction(_ sender: Any) {
+        showDefaultAlert(title: "Atención", message: "Acción de cambiar dirección en desarrollo", view: self)
+    }
+    
+    @IBAction func btnFilterAction(_ sender: Any) {
+        showDefaultAlert(title: "Atención", message: "Acción de filtrar en desarrollo", view: self)
+    }
+    
 }
--- ChallengeLibre/SearchProduct/SearchProductViewController.swift
+++ ChallengeLibre/SearchProduct/SearchProductViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SearchProductViewController: UIViewController, UISearchBarDelegate, UITextViewDelegate {
+class SearchProductViewController: UIViewController, UISearchBarDelegate {
     
     //Components search view
     @IBOutlet weak var txtSearch: UISearchBar!
@@ -15,12 +15,12 @@ class SearchProductViewController: UIViewController, UISearchBarDelegate, UIText
     @IBOutlet weak var iconShoppingCar: UIButton!
     @IBOutlet weak var stackViewAddressData: UIStackView!
     
-    @IBOutlet weak var viewQuantitySearch: UIView!
-    @IBOutlet weak var labelQuantitySearch: UILabel!
-    
     @IBOutlet weak var stackDataView: UIStackView!
     @IBOutlet weak var viewDashboard: UIView!
     @IBOutlet weak var viewRecentSearches: UIView!
+    
+    var isSearchAgain = false
+    var txtTitleSearch = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,10 +32,28 @@ class SearchProductViewController: UIViewController, UISearchBarDelegate, UIText
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        viewDashboard.isHidden = false
-        viewRecentSearches.isHidden = true
-        viewQuantitySearch.isHidden = true
-        
+        if isSearchAgain {
+            txtSearch.text = txtTitleSearch
+            txtSearch.becomeFirstResponder()
+            
+            iconShoppingCar.isHidden = true
+            btnCancel.isHidden = false
+            stackViewAddressData.isHidden = true
+            
+            viewDashboard.isHidden = true
+            viewRecentSearches.isHidden = false
+        }else{
+            
+            txtSearch.text = ""
+            iconShoppingCar.isHidden = false
+            btnCancel.isHidden = true
+            stackViewAddressData.isHidden = false
+            txtSearch.endEditing(true)
+            
+            viewDashboard.isHidden = false
+            viewRecentSearches.isHidden = true
+            txtSearch.text = ""
+        }
         
     }
     
@@ -60,39 +78,61 @@ class SearchProductViewController: UIViewController, UISearchBarDelegate, UIText
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         if searchBar == txtSearch {
             print("dio en buscar: \(txtSearch.text!)")
-            iconShoppingCar.isHidden = false
-            btnCancel.isHidden = true
-            txtSearch.endEditing(true)
             
-            viewDashboard.isHidden = true
-            viewRecentSearches.isHidden = true
-            viewQuantitySearch.isHidden = false
+            if isSearchAgain {
+                
+                self.dismiss(animated: false)
+                
+            }else {
+                
+                let storyBoard = UIStoryboard(name: "ListProductsView", bundle: nil)
+                let controller = storyBoard.instantiateViewController(withIdentifier: "ListProductsViewController") as! ListProductsViewController
+                controller.txtTitleSearch = self.txtSearch.text!
+                self.present(controller, animated: true, completion: nil)
+                
+            }
             
         }
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        if self.isBeingDismissed {
+            if isSearchAgain {
+                
+                self.isSearchAgain = false
+                
+                let storyBoard = UIStoryboard(name: "ListProductsView", bundle: nil)
+                let controller = storyBoard.instantiateViewController(withIdentifier: "ListProductsViewController") as! ListProductsViewController
+                controller.txtTitleSearch = self.txtSearch.text!
+                self.presentingViewController?.present(controller, animated: true, completion: nil)
+            }
+        }
+    }
+    
     @IBAction func btnCancelarAction(_ sender: Any) {
-        txtSearch.text = ""
-        iconShoppingCar.isHidden = false
-        btnCancel.isHidden = true
-        stackViewAddressData.isHidden = false
-        txtSearch.endEditing(true)
         
-        viewDashboard.isHidden = false
-        viewRecentSearches.isHidden = true
+        if isSearchAgain {
+            
+            self.dismiss(animated: true)
+            
+        } else {
+            txtSearch.text = ""
+            iconShoppingCar.isHidden = false
+            btnCancel.isHidden = true
+            stackViewAddressData.isHidden = false
+            txtSearch.endEditing(true)
+            
+            viewDashboard.isHidden = false
+            viewRecentSearches.isHidden = true
+        }
+        
     }
     
     @IBAction func btnIconCar(_ sender: Any) {
-        let storyBoard = UIStoryboard(name: "DetailProductView", bundle: nil)
         
-        let controller = storyBoard.instantiateViewController(withIdentifier: "DetailProductViewController") as! DetailProductViewController
         
-        self.present(controller, animated: true, completion: nil)
     }
     
-    @IBAction func btnFilterAction(_ sender: Any) {
-        showDefaultAlert(title: "Atención", message: "Acción de filtrar en desarrollo", view: self)
-    }
 }
 
 
